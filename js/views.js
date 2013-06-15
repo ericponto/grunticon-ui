d81ae49f6dfa8386551024bfeb61c31470e014db
Eric Ponto <eric.ponto@gmail.com>
2013-06-14 22:36:31 -0500

    Added support for preview.html
    Still need to add in the real image sizes

--- js/views.js
+++ js/views.js
@@ -111,6 +111,7 @@ define([
 		svgTemplate: _.template($("#svg-css-template").html()),
 		pngTemplate: _.template($("#png-css-template").html()),
 		fallbackTemplate: _.template($("#fallback-css-template").html()),
+		exampleTemplate: _.template($("#example-html-template").html()),
 		iconTemplate: _.template($("#svg-icons-template").html()),
 		events: {
 			"click #download": "download"
@@ -120,6 +121,7 @@ define([
 				svgHtml = "",
 				pngHtml = "",
 				fallbackHtml = "",
+				exampleHtml = "",
 				iconHtml = "";
 
 			$.when(view.collection.isRead).done(function(){ 
@@ -128,13 +130,15 @@ define([
 						svgHtml += view.svgTemplate(data);
 						pngHtml += view.pngTemplate(data);
 						fallbackHtml += view.fallbackTemplate(data);
+						exampleHtml += view.exampleTemplate(data);						
 						iconHtml += view.iconTemplate(data);
 					});
 
-				view.$el.find("#svg-results").html(svgHtml);
-				view.$el.find("#png-results").html(pngHtml);
-				view.$el.find("#fallback-results").html(fallbackHtml);
-				view.$el.find("#svg-icons-results").html(iconHtml);
+				view.$("#svg-results").html(svgHtml);
+				view.$("#png-results").html(pngHtml);
+				view.$("#fallback-results").html(fallbackHtml);
+				view.$("#example-html-results").append(exampleHtml.replace(/</g, "&lt;") + "\n&lt;/body\n>&lt;/html>");
+				view.$("#svg-icons-results").html(iconHtml);
 				view.$el.fadeIn();
 
 			});
@@ -153,6 +157,7 @@ define([
 				zip.file("icons.data.png.css", view.$("#png-results").text());
 				zip.file("icons.fallback.css", view.$("#png-results").text());
 				zip.file("grunticon.loader.txt", '<!-- Unicode CSS Loader: place this in the head of your page -->\n<script>\n/* grunticon Stylesheet Loader | https://github.com/filamentgroup/grunticon | (c) 2012 Scott Jehl, Filament Group, Inc. | MIT license. */\nwindow.grunticon=function(e){if(e&&3===e.length){var t=window,n=!!t.document.createElementNS&&!!t.document.createElementNS("http://www.w3.org/2000/svg","svg").createSVGRect&&!!document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1"),A=function(A){var o=t.document.createElement("link"),r=t.document.getElementsByTagName("script")[0];o.rel="stylesheet",o.href=e[A&&n?0:A?1:2],r.parentNode.insertBefore(o,r)},o=new t.Image;o.onerror=function(){A(!1)},o.onload=function(){A(1===o.width&&1===o.height)},o.src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="}};\ngrunticon( [ "/example/output/icons.data.svg.css", "/example/output/icons.data.png.css", "/example/output/icons.fallback.css" ] );</script>\n<noscript><link href="/example/output/icons.fallback.css" rel="stylesheet"></noscript>');
+				zip.file("preview.html", view.$("#example-html-results").text());			
 
 			var content = zip.generate();
 
